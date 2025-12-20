ff5bf379d3b2b47263f61b5d2df394149a4005ab
CorgiChen <qq882517@gmail.com>
2025-12-20 08:05:02 +0800

    stage select

---
+++ Assets/Script/sceneTransport.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneTransport : MonoBehaviour
+{
+    public string sceneName; // 要切換的場景名稱
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
